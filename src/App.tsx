8be99a19a7b09f58aca3d4a842720dc316a8f843
Muhammad-ammar321 <mammarshamim@gmail.com>
2025-09-22 00:49:25 +0500

    error fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import StudentsList from './components/StudentList'
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router'
-import './app.css'
+import './index.css'
 import { store } from './app/store'
 import router from './router/router'
 
