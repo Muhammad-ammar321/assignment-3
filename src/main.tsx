503cb656fb3c42140b0c28b2bd26a4a2d31d8f47
Muhammad-ammar321 <mammarshamim@gmail.com>
2025-09-22 00:42:34 +0500

    all done

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router'
 import './app.css'
 import { store } from './app/store'
-import router from './routes/router'
+import router from './router/router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,15 +12,4 @@ createRoot(document.getElementById('root')!).render(
       <RouterProvider router={router} />
     </Provider>
   </StrictMode>
-=======
-// import './index.css'
-// import App from './App.tsx'
-import { RouterProvider } from 'react-router'
-import { Routes } from './router/routes.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={Routes} />
-  </StrictMode>,
->>>>>>> 24b606852aa9f3bd67e0c4e6fbdc57f7a2cc5765
 )
